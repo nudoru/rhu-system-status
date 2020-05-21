dd539c3d210e61bd4c77720c64880a97cad81531
Matt Perkins <hello@mattperkins.me>
2020-05-21 11:53:04 -0400

    extract header nav to new component and add mobile menu

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -1,53 +1,29 @@
 <style lang="scss" scoped></style>
 
 <template>
-    <section role="banner" class="app-header">
-        <div class="container-horizontal">
-            <div>
-                <slot></slot>
-            </div>
-            <div class="end">
-                <nav class="app-navigation-primary">
-                    <ul class="">
-                        <li class="note"><i class="fa fa-info-circle"></i>Nav is for dev only</li>
-                        <li :class="{'active': routeRoot === splitRoute(route.path)}" v-for="(route, idx) in this.$router.options.routes" :key="idx">
-                            <router-link class="" :to="route.path" >
-                                {{route.name}}
-                            </router-link>
-                        </li>
-                    </ul>
-                </nav>
-            </div>
-        </div>
-    </section>
-
+  <section role="banner" class="app-header">
+    <div class="container-horizontal">
+      <div>
+        <slot></slot>
+      </div>
+      <div class="end">
+        <AppNavigation />
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
-  export default {
-    name   : 'Header',
-    props  : [],
-    data() {
-      return {};
-    },
-    methods: {
-      splitRoute(r) {
-        // get the "root" without any params
-        return r.split('/')[1]
-      }
-    },
-    computed: {
-      // get the "root" without any params
-      routeRoot: function() {
-        return this.splitRoute(this.$route.path);
-      }
-    },
-    mounted() {
-      // console.log('mount header route is',this.$router.options.routes);
-      // console.log(this.$route.path, this.route);
-    },
-    beforeUpdate() {
-      // console.log('updated header route is',this.$route);
-    }
-  };
+import AppNavigation from "../ui/AppNavigation";
+
+export default {
+  name: "Header",
+  components: { AppNavigation },
+  props: [],
+  data() {
+    return {};
+  },
+  methods: {},
+  computed: {},
+};
 </script>
---
+++ src/components/ui/AppNavigation.vue
@@ -0,0 +1,42 @@
+<style lang="scss" scoped></style>
+
+<template>
+  <nav class="app-navigation-primary">
+    <!--    <div class="mobile-toggle">-->
+    <label class="mobile-toggle" for="nav-mobile-toggle"
+      ><i class="fa fa-bars"></i
+    ></label>
+    <input type="checkbox" id="nav-mobile-toggle" />
+    <!--    </div>-->
+    <ul class="">
+      <li class="note"><i class="fa fa-info-circle"></i>Nav is for dev only</li>
+      <li
+        :class="{ active: routeRoot === splitRoute(route.path) }"
+        v-for="(route, idx) in this.$router.options.routes"
+        :key="idx"
+      >
+        <router-link class="" :to="route.path">
+          {{ route.name }}
+        </router-link>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "AppNavigation",
+  methods: {
+    splitRoute(r) {
+      // get the "root" without any params
+      return r.split("/")[1];
+    },
+  },
+  computed: {
+    // get the "root" without any params
+    routeRoot: function () {
+      return this.splitRoute(this.$route.path);
+    },
+  },
+};
+</script>
