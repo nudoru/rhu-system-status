5d3a19e0a90f5cdc27e448d4b86f9cf92db005f2
Matt Perkins <hello@mattperkins.me>
2020-05-21 16:16:32 -0400

    animated mobile menu

--- src/components/ui/AppNavigation.vue
+++ src/components/ui/AppNavigation.vue
@@ -1,12 +1,12 @@
 <style lang="scss" scoped></style>
 
 <template>
-  <nav class="app-navigation-primary">
-    <label class="mobile-toggle" for="nav-mobile-toggle"
-      ><i class="fa fa-bars"></i
-    ></label>
-    <input type="checkbox" id="nav-mobile-toggle" />
-    <div class="navigation-list">
+  <div :class="navCls">
+    <button class="mobile-toggle" @click="toggleMobileNav">
+      <i v-if="!mobileNavOpen" class="fa fa-bars"></i>
+      <i v-if="mobileNavOpen" class="fa fa-times"></i>
+    </button>
+    <nav class="navigation-list">
       <ul class="">
         <li class="note">
           <i class="fa fa-info-circle"></i>Nav is for dev only
@@ -15,29 +15,47 @@
           :class="{ active: routeRoot === splitRoute(route.path) }"
           v-for="(route, idx) in this.$router.options.routes"
           :key="idx"
+          @click="routeClick"
         >
           <router-link class="" :to="route.path">
             {{ route.name }}
           </router-link>
         </li>
       </ul>
-    </div>
-  </nav>
+    </nav>
+  </div>
 </template>
 
 <script>
 export default {
   name: "AppNavigation",
+  data() {
+    return {
+      mobileNavOpen: false,
+    };
+  },
   methods: {
     splitRoute(r) {
       // get the "root" without any params
       return r.split("/")[1];
+    },
+    routeClick() {
+      this.mobileNavOpen = false;
+    },
+    toggleMobileNav() {
+      this.mobileNavOpen = !this.mobileNavOpen;
     },
   },
   computed: {
     // get the "root" without any params
     routeRoot: function () {
       return this.splitRoute(this.$route.path);
+    },
+    navCls: function () {
+      return [
+        "app-navigation-primary",
+        this.mobileNavOpen ? "open" : "closed",
+      ].join(" ");
     },
   },
 };
